5b60acb5d212aa06dbf533a2368d49df57487b69
Hung <dvhung@IfiSolution.local>
2020-06-24 18:47:03 +0700

    update

--- example/element.js
+++ example/element.js
@@ -1,8 +1,10 @@
 import '../src/nttdi-nowex-users-table';
+import '../src/nttdi-nowex-users-detail'
 
 const el = document.createElement('DIV');
 document.body.appendChild(el);
 
 el.innerHTML = `		
 <nttdi-nowex-users-table></nttdi-nowex-users-table>
+<nttdi-nowex-users-detail></nttdi-nowex-users-detail>
 `;
